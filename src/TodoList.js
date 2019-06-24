fc6850332f8d6a2509a60da9d7548fac12b9938e
DaveFres <nanyandave@gmail.com>
2019-06-24 20:10:16 +0300

    add remove functionality

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,23 +6,37 @@ class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            todos: [{ task: "Walk" }, { task: "Catch a fish" }]
+            todos: []
         };
         this.create = this.create.bind(this);
+        this.remove = this.remove.bind(this);
     }
 
     create(newTodo) {
         this.setState({
             todos: [...this.state.todos, newTodo]
-        })
+        });
+    }
+
+    remove(id) {
+        this.setState({
+            todos: this.state.todos.filter(t => t.id !== id)
+        });
     }
 
     render() {
-        const todos = this.state.todos.map(todo => <Todo task={todo.task} />);
+        const todos = this.state.todos.map(todo => (
+            <Todo
+                key={todo.id}
+                id={todo.id}
+                task={todo.task}
+                removeTodo={this.remove}
+            />
+        ));
         return (
             <div>
                 <h1>Todo List</h1>
-                <NewTodoForm createTodo={this.create}/>
+                <NewTodoForm createTodo={this.create} />
                 <ul>{todos}</ul>
             </div>
         );
