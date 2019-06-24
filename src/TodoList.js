cbe7cb5db9118c42c42d536ff51da297e98dd0d2
DaveFres <nanyandave@gmail.com>
2019-06-24 19:11:19 +0300

    add components: todolist, todo

---
+++ src/TodoList.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Todo from "./Todo";
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [{ task: "Walk"}, {task: "Catch a fish" }]
+        };
+    }
+
+    render() {
+        const todos = this.state.todos.map(todo => <Todo task={todo.task} />);
+        return (
+            <div>
+                <h1>Todo List</h1>
+                <ul>{todos}</ul>
+            </div>
+        );
+    }
+}
+
+export default TodoList;
