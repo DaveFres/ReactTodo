3ecd3f3035cd1da009cb661b02f5f64aca487cf6
DaveFres <nanyandave@gmail.com>
2019-06-25 11:32:34 +0300

    Updates

---
+++ precache-manifest.fac57a1e204767a710b321eb70c5daf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28c2fc133ce8035e8eb5993a706dfb1",
+    "url": "/ReactTodo/index.html"
+  },
+  {
+    "revision": "91de960fb8841222bf0c",
+    "url": "/ReactTodo/static/css/main.3f37da00.chunk.css"
+  },
+  {
+    "revision": "73b3b7de436b87a9df8b",
+    "url": "/ReactTodo/static/js/2.86706d93.chunk.js"
+  },
+  {
+    "revision": "91de960fb8841222bf0c",
+    "url": "/ReactTodo/static/js/main.7403e5a4.chunk.js"
+  },
+  {
+    "revision": "58ea5603444b815de944",
+    "url": "/ReactTodo/static/js/runtime~main.b7112dbe.js"
+  }
+]);
